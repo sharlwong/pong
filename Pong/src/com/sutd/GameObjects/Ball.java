b989b31fd5e4519858874df64b3351d71549a5ad
Tianchi Wang <tianchiing@gmail.com>
2014-03-27 14:48:59 +0800

    Add functions to detect wall reflection and make changes to velocity

--- Pong/src/com/sutd/GameObjects/Ball.java
+++ Pong/src/com/sutd/GameObjects/Ball.java
@@ -12,9 +12,11 @@ public class Ball {
 	private Vector2D startVelocity;
 	private long tempCurrentMillis;
 
-	public Ball(Vector2D startPosition, Vector2D startVelocity, long realStartTime) {
+	public Ball(Vector2D startPosition, Vector2D startVelocity,
+			long realStartTime) {
 		this.startPosition = startPosition;
-		this.startVelocity = startVelocity.makeUnitVector().multiply(Assets.BALL_SPEED);
+		this.startVelocity = startVelocity.makeUnitVector().multiply(
+				Assets.BALL_SPEED);
 		this.realStartTimeMillis = realStartTime;
 	}
 
@@ -27,28 +29,47 @@ public class Ball {
 		Vector2D youAreHere = new Vector2D(startPosition);
 		youAreHere.add(realVelocity.multiply(timeTravelled));
 		while (youAreHere.x < 0 || youAreHere.x > Assets.WIDTH) {
-			if (youAreHere.x < 0) youAreHere.x = 0 - youAreHere.x;
-			if (youAreHere.x > Assets.WIDTH) youAreHere.x = 2 * Assets.WIDTH - youAreHere.x;
+			if (youAreHere.x < 0)
+				youAreHere.x = 0 - youAreHere.x;
+			if (youAreHere.x > Assets.WIDTH)
+				youAreHere.x = 2 * Assets.WIDTH - youAreHere.x;
 		}
 		return youAreHere;
 	}
 
-	public Vector2D getCurrentPosition(){
+	public Vector2D getCurrentPosition() {
 		return getPosition(tempCurrentMillis);
 	}
 
 	public void init(long fakeStartTime) throws RuntimeException {
 		this.apparentStartTimeMillis = fakeStartTime;
-		double distanceToTravel = Assets.HEIGHT * startVelocity.length() / Math.abs(startVelocity.y);
+		double distanceToTravel = Assets.HEIGHT * startVelocity.length()
+				/ Math.abs(startVelocity.y);
 		double realTimeTakenMillis = distanceToTravel / Assets.BALL_SPEED;
 		realEndTimeMillis = realStartTimeMillis + (long) realTimeTakenMillis;
-		if ((apparentStartTimeMillis - realStartTimeMillis) > Assets.MAX_ACCEPTABLE_LAG) throw new Assets.LagException();
+		if ((apparentStartTimeMillis - realStartTimeMillis) > Assets.MAX_ACCEPTABLE_LAG)
+			throw new Assets.LagException();
 
-		double realSpeed = distanceToTravel / (realEndTimeMillis - apparentStartTimeMillis);
+		double realSpeed = distanceToTravel
+				/ (realEndTimeMillis - apparentStartTimeMillis);
 		realVelocity = startVelocity.makeUnitVector().multiply(realSpeed);
 	}
 
-	public Ball paddleReflect(Vector2D newVelocity){
-		return new Ball(getCurrentPosition(),newVelocity, tempCurrentMillis);
+	public Ball paddleReflect(Vector2D newVelocity) {
+		return new Ball(getCurrentPosition(), newVelocity, tempCurrentMillis);
+	}
+
+	public boolean checkWallReflect() {
+		if (getCurrentPosition().x <= 0 || getCurrentPosition().x >= 1) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public Ball wallReflect() {
+		Vector2D newVelocity = new Vector2D(realVelocity.x * (-1),
+				realVelocity.y);
+		return new Ball(getCurrentPosition(), newVelocity, tempCurrentMillis);
 	}
 }
