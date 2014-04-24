6d1b6b04043f38d4a9304234cc0774e1415228f4
averykhoo <averykhoo@gmail.com>
2014-04-24 14:30:54 +0800

    minor edits
    does everything still work?
    i haven't seen a run since 6 commits ago, so just checking

--- Pong/src/com/sutd/GameObjects/BallTest.java
+++ Pong/src/com/sutd/GameObjects/BallTest.java
@@ -13,73 +13,114 @@ import com.sutd.PongHelpers.Vector2D;
 
 public class BallTest {
 
-	Ball testBall;
-	int ballType;
-	Vector2D position;
-	
-	public BallTest(){
-		SecureRandom random;
-		long elapsedTimeMillis;
-		Vector2D speed;
-		elapsedTimeMillis = 0;
-		random = new SecureRandom();
-		random.setSeed(1234567890);
-		/* ball type and other data */
-		double randomValue = random.nextDouble() * 14;
-		ballType = (int) randomValue;
-
-		/* starting position */
-		position = new Vector2D(Constants.WIDTH / 2, Constants.HEIGHT / 2);
-
-		/* randomize starting velocity within 45 degree */
-		double speed1 = random.nextDouble() - 0.5;
-		double speed2 = random.nextDouble() - 0.5;
-		if (Math.abs(speed1) < Math.abs(speed2))
-			speed = new Vector2D(speed1, speed2);
-		else
-			speed = new Vector2D(speed2, speed1);
-		testBall = new Ball(position, speed, elapsedTimeMillis, ballType);
+	private void pass(String methodName) {
+		System.out.println("PASS: " + methodName);
 	}
 
 	@Test
-	public void testGetType() {
-		assertEquals(ballType, testBall.getType());
+	public void testGetSpeedMultiplier() throws Exception {
+		Ball ball;
+		for (int i = -10; i < 2000; i++) {
+			ball = new Ball(Vector2D.X, Vector2D.Y, 0, 0, (double) i / 100);
+			if (i > 0) assert ball.getSpeedMultiplier() == (double) i / 100;
+			assert ball.getSpeedMultiplier() > 0;
+		}
+		pass("getSpeedMultiplier");
 	}
-	
-	@Test
-	public void getCurrentPosition(){
-		/* start position is current position if no update happened */
-		assertEquals(position, testBall.getCurrentPosition());
-		
-		/* start position is current position if update 0 millisecond */
-		testBall.updateCurrentTime(0);
-		assertEquals(position, testBall.getCurrentPosition());
-	}
-	
+
 	@Test
 	public void testMovement() {
-		Ball testBall2 = new Ball(new Vector2D(0, 0), new Vector2D(0, 1), 0, 1);
-		testBall2.updateCurrentTime(10);
-		/* startvelocity only represent direction, while the speed is always 0.0005 */
-		Vector2D expectedPosition = new Vector2D(0, Constants.BALL_SPEED * 10);
-		System.out.println(testBall2.getCurrentPosition().y);
-		assertEquals(expectedPosition, testBall2.getCurrentPosition());
-		testBall2 = new Ball(new Vector2D(0, 0), new Vector2D(1, 0), 0, 1);
-		testBall2.updateCurrentTime(10);
-		/* error input handler test, if velocity in y direction is 0,  */
-		assertEquals(expectedPosition, testBall2.getCurrentPosition());
+		for (int t = -100; t < 200; t++) {
+			Ball testBall2 = new Ball(Vector2D.ZERO, Vector2D.Y.cpy(), t, 0);
+			Ball testBall3 = new Ball(Vector2D.ZERO, Vector2D.X.cpy(), t, 0);
+			Vector2D expectedPosition = null;
+			for (int i = -100; i < 2000; i++) {
+				testBall2.updateCurrentTime(i);
+				testBall3.updateCurrentTime(i);
+				expectedPosition = new Vector2D(0, Constants.BALL_SPEED * (i - t));
+				if (t <= i) {
+					assert expectedPosition.equals(testBall2.getCurrentPosition());
+					assert expectedPosition.equals(testBall3.getCurrentPosition());
+				}
+				else {
+
+					assert !expectedPosition.equals(testBall2.getCurrentPosition());
+					assert !expectedPosition.equals(testBall3.getCurrentPosition());
 	}
-	
+			}
+		}
+
+		pass("movement");
+	}
+
 	@Test
-	public void testIsMovingUp() {
-		Ball testBall2 = new Ball(new Vector2D(0, 0), new Vector2D(0, 1), 0, 1);
-		assertTrue(testBall2.isMovingUp());
-		testBall2 = new Ball(new Vector2D(0, 0), new Vector2D(0, -1), 0, 1);
-		assertFalse(testBall2.isMovingUp());
-		testBall2 = new Ball(new Vector2D(0, 0), new Vector2D(0, 0), 0, 1);
-		/* input error handler */
-		assertTrue(testBall2.isMovingUp());
+	public void testGetCurrentPosition() throws Exception {
+		Ball ball;
+		for (int i = 0; i < 99; i++) {
+			for (int j = 0; j < 99; j++) {
+				Vector2D vector2D = Vector2D.X.cpy().multiply((double) i / 100).add(Vector2D.Y.cpy().multiply((double) j / 100));
+				ball = new Ball(vector2D, Vector2D.Y, 0, 0);
+				ball.updateCurrentTime(0);
+				assert ball.getCurrentPosition().equals(vector2D);
+	}
+		}
+		pass("getCurrentPosition");
 	}
 
+	@Test
+	public void testGetScore() throws Exception {
+		Ball ball;
+		for (int i = 0; i < 99; i++) {
+			ball = new Ball(Vector2D.X, Vector2D.Y, 0, i);
+			assert ball.getScore() == i + 1;
+		}
+		pass("getScore");
+	}
 
+	@Test
+	public void testGetType() throws Exception {
+		Ball ball;
+		for (int i = 0; i < 99; i++) {
+			ball = new Ball(Vector2D.X, Vector2D.Y, 0, i);
+			assert ball.getType() == i;
+	}
+		pass("getType");
+	}
+
+	@Test
+	public void testIsMovingUp() throws Exception {
+		Ball ball;
+		for (int i = 0; i < 99; i++) {
+			for (int j = 0; j < 99; j++) {
+				Vector2D vector2D = Vector2D.X.cpy().multiply((double) i / 100).add(Vector2D.Y.cpy().add(0, -0.5).multiply((double) j / 100));
+				ball = new Ball(Vector2D.X, vector2D, 0, 0);
+				ball.updateCurrentTime(0);
+				if (vector2D.y >= 0) assert ball.isMovingUp();
+				else assert !ball.isMovingUp();
+	}
+		}
+		pass("isMovingUp");
+	}
+
+	@Test
+	public void testUpdating() throws Exception {
+		Ball ball;
+		for (int i = 0; i < 99; i++) {
+			for (int j = 0; j < 99; j++) {
+				Vector2D vector2D = Vector2D.X.cpy().multiply((double) i / 100).add(Vector2D.Y.cpy().add(0, -0.5).multiply((double) j / 100));
+				ball = new Ball(vector2D, vector2D, 0, 0);
+				for (int k = -10; k < 2000; k++) {
+					ball.updateCurrentTime(k * 7);
+					if (ball.getCurrentPosition().y <= Constants.HEIGHT + Constants.PADDLE_EFFECTIVE_DEPTH + Constants.EDGE_PADDING && ball.getCurrentPosition().y >= 0 - Constants.PADDLE_EFFECTIVE_DEPTH - Constants.EDGE_PADDING)
+						assert ball.inGame();
+					else assert !ball.inGame();
+					assert ball.getCurrentPosition().x >= 0;
+					assert ball.getCurrentPosition().x <= 1;
+	}
+			}
+		}
+		pass("inGame");
+		pass("updateCurrentTime");
+		pass("getCurrentPosition");
+	}
 }
