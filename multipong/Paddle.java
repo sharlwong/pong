eb11a906aaa93e8295ff748e695e0896021cdde0
averykhoo <averykhoo@gmail.com>
2014-04-25 12:25:36 +0800

    comments and magic
    yes, more magic

--- multipong/Paddle.java
+++ multipong/Paddle.java
@@ -7,6 +7,11 @@ public class Paddle {
 	private Vector2D paddleCenter;
 	public  boolean  playerBottom;
 
+	/**
+	 * give you a paddle
+	 *
+	 * @param playerNum top == 1 OR bottom == 0
+	 */
 	public Paddle(int playerNum) {
 		this.max = Constants.WIDTH + Constants.BALL_RADIUS - (Constants.PADDLE_WIDTH / 2);
 		this.min = (Constants.PADDLE_WIDTH / 2) - Constants.BALL_RADIUS;
@@ -15,17 +20,29 @@ public class Paddle {
 		this.playerBottom = (playerNum == 0);
 	}
 
-	public Ball bounce(Ball b, long currentTimeMillis) {
+	/**
+	 * bounce a ball off this paddle
+	 *
+	 * @param ball              to bounce
+	 * @param currentTimeMillis give the ball a birth date
+	 * @return new ball to relace the old one
+	 */
+	public Ball bounce(Ball ball, long currentTimeMillis) {
 		double yVelocity = Constants.PADDLE_WIDTH / 2;
 		if (!playerBottom) yVelocity = 0 - yVelocity;
-		Vector2D outVelocity = new Vector2D(b.getCurrentPosition().x - paddleCenter.x, yVelocity);
-		outVelocity.makeUnitVector().multiply(Constants.BALL_SPEED);
-		return new Ball(b.getCurrentPosition(), outVelocity, currentTimeMillis, b.getType(), b.getSpeedMultiplier());
+		Vector2D outVelocity = new Vector2D(ball.getCurrentPosition().x - paddleCenter.x, yVelocity);
+		return new Ball(ball.getCurrentPosition(), outVelocity, currentTimeMillis, ball.getType(), ball.getSpeedMultiplier());
 	}
 
-	public boolean collisionCheck(Ball b) {
-		Vector2D ballPosition = b.getCurrentPosition();
-		boolean up = b.isMovingUp();
+	/**
+	 * is this ball hitting this paddle
+	 *
+	 * @param ball to check
+	 * @return yes or no
+	 */
+	public boolean collisionCheck(Ball ball) {
+		Vector2D ballPosition = ball.getCurrentPosition();
+		boolean up = ball.isMovingUp();
 		if (Math.abs(ballPosition.x - paddleCenter.x) > (Constants.PADDLE_WIDTH / 2)) return false;
 		if (playerBottom && ballPosition.y < (0 - Constants.PADDLE_EFFECTIVE_DEPTH) && !up) return false;
 		if (playerBottom && ballPosition.y <= 0 && !up) return true;
@@ -34,29 +51,48 @@ public class Paddle {
 		return false;
 	}
 
-	public void incrementScore(Ball b) {
-		score += b.getScore();
+	/**
+	 * add a balls weight to your score
+	 *
+	 * @param ball to add
+	 */
+	public void incrementScore(Ball ball) {
+		score += ball.getScore();
 	}
 
+	/**
+	 * move to this place
+	 *
+	 * @param xValue move here
+	 */
 	private void setPosition(double xValue) {
 		paddleCenter.x = xValue;
 		if (paddleCenter.x < min) paddleCenter.x = min;
 		if (paddleCenter.x > max) paddleCenter.x = max;
 	}
 
+	/**
+	 * move relative to the traversal line
+	 *
+	 * @param fraction of the line the paddle should be at
+	 */
 	public void setFractionalPosition(double fraction) {
 		if (fraction < 0 || fraction > 1) return;
 		setPosition(min + fraction * (max - min));
 	}
 
 	/**
-	 * @return paddle center coordinates.
+	 * where art thou (i want numbers)
+	 *
+	 * @return here i am (paddle center
 	 */
 	public double[] getXY() {
 		return new double[]{paddleCenter.x, paddleCenter.y};
 	}
 
 	/**
+	 * request vector position
+	 *
 	 * @return paddle center coordinates in the form of Vector2D
 	 */
 	public Vector2D getCenter() {
@@ -64,12 +100,19 @@ public class Paddle {
 	}
 
 	/**
+	 * whats your score
+	 *
 	 * @return current score of the paddle controller
 	 */
 	public int getScore() {
 		return score;
 	}
 
+	/**
+	 * where are you
+	 *
+	 * @return this is fraction of where i could be
+	 */
 	public double getFractionalPosition() {
 		return (paddleCenter.x - min) / (max - min);
 	}
